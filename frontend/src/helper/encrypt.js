dd75570f23ddf39218c26b299618d9fbdcb27065
Hariharan Sivakumar <hariharansivakumar@affordmed.com>
2024-11-18 03:08:46 -0500

    use node forge

--- frontend/src/helper/encrypt.js
+++ frontend/src/helper/encrypt.js
@@ -1,9 +1,15 @@
-import JSEncrypt from 'jsencrypt';
+import forge from 'node-forge';
 
 const encrypt = (valueToEncrypt) => {
-    const jsEncrypt = new JSEncrypt();
-    jsEncrypt.setPublicKey(atob(process.env.REACT_APP_PASSWORD_ENC_PUBLIC_KEY));
-    return jsEncrypt.encrypt(valueToEncrypt);
+    // Import the public key (in PEM format)
+    const publicKeyPem = atob(process.env.REACT_APP_PUBLIC_KEY);
+    const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
+
+    // Encrypt the value with RSA-OAEP and specify SHA-256 as the hash algorithm
+    const encrypted = publicKey.encrypt(valueToEncrypt, 'RSA-OAEP', { md: forge.md.sha256.create() });
+
+    // Return the encrypted value in Base64 format
+    return forge.util.encode64(encrypted);
 };
 
 export default encrypt;
